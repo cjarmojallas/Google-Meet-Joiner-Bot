d9e38d4fa5a9f3c1017e2f0dc7ae4f7cb580c564
cjarmojallas <69428636+cjarmojallas@users.noreply.github.com>
2021-04-08 14:59:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,24 +16,29 @@ current_time = now.strftime("%H:%M:%S")
 
 
 def autoconnect(link):
+    #change the directory to your chrome directory
     chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
     webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
     webbrowser.get('chrome').open_new(link)
 
     x = 10
     time.sleep(x)
-
+    
+    #turning off camera
     pyautogui.hotkey('ctrl', 'e')
 
     time.sleep(2)
-
+    
+    #turning off microphonne
     pyautogui.hotkey('ctrl', 'd')
 
     x = 3
     time.sleep(x)
-
+    
+    #joing the meeting
     pyautogui.click(1320, 580)
-
+    
+    #will end the meeting after the given duration is done
     time.sleep(duration * 60)
 
     pyautogui.hotkey('alt', 'f4')
