86fbb16be5e77d043f0c043eb1aa1d0dab374f16
cjarmojallas <69428636+cjarmojallas@users.noreply.github.com>
2021-04-08 14:54:52 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+
+import pyautogui
+import time
+import webbrowser
+from datetime import datetime
+from os import system
+
+clock = input("Start of meeting?: ")
+url = input("enter meeting url: ")
+print("End of Meeting")
+print("Put Minutes Please")
+duration = int(input("duration of meeting:? "))
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+
+def autoconnect(link):
+    chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+    webbrowser.get('chrome').open_new(link)
+
+    x = 10
+    time.sleep(x)
+
+    pyautogui.hotkey('ctrl', 'e')
+
+    time.sleep(2)
+
+    pyautogui.hotkey('ctrl', 'd')
+
+    x = 3
+    time.sleep(x)
+
+    pyautogui.click(1320, 580)
+
+    time.sleep(duration * 60)
+
+    pyautogui.hotkey('alt', 'f4')
+
+while current_time != (clock):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    system("cls")
+
+if clock == current_time:
+    autoconnect(url)
+
+
+
+
+
+
+
